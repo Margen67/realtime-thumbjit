648c5b3efaf09d0f26a2febb6bbbc5ddc8a57103
chris <chss95cs@gmail.com>
2018-02-18 00:28:59 -0500

    basic templating

--- asmtemplate.hpp
+++ asmtemplate.hpp
@@ -3,7 +3,7 @@ namespace templating {
 
     
     enum class Register_e : Opcodes::insn_size_t {
-        r0,
+        r0 =0,
         r1,
         r2,
         r3,
@@ -158,10 +158,18 @@ namespace templating {
     }
 
     struct KnowableState {
-        uword_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, sp, lr, pc;
         unsigned char* priorbyte;
         uword_t npriorbytes;
+        uword_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, sp, lr, pc;
+        uword_t known_mask;
+
+        bool is_known(Register_e reg) {
+            return (known_mask & (1 << acceptEnumClass(reg))) != 0;
+        }
     };
+
+
+
     using AbstractResolver_t = insn_t (*)(KnowableState*);
 
     class AbstractMarker {
@@ -172,7 +180,18 @@ namespace templating {
         constexpr AbstractMarker(uword_t code_index, AbstractResolver_t resolver) 
         : m_code_index(code_index),
         m_resolver(resolver) {}
+
+        inline insn_t operator ()(KnowableState* state) {
+            return m_resolver(state);
+        }
+
+        inline bool operator == (uword_t idx) {
+            return idx == m_code_index;
+        }
+
     };
+
+
 
     constexpr uword_t MAX_TEMPLATE_INSNS = 256;
     constexpr uword_t MAX_ABSTRACT_MARKERS = 32;
@@ -183,20 +202,122 @@ namespace templating {
         uword_t currentAbstractCount;
 
         bool waiting_for_abstract_handler;
+        uword_t last_abstract_pos;
     public:
         constexpr InsnStream() : code(), position(0), markers(), currentAbstractCount(0),
-        waiting_for_abstract_handler(false){}
+        waiting_for_abstract_handler(false),
+        last_abstract_pos(-1){}
 
 
 
         constexpr InsnStream& operator <<(insn_t insn) {
+            CE_ASSERT(!waiting_for_abstract_handler, "Previous generated instruction was abstract, but no handler was ever provided");
+            CE_ASSERT(position < MAX_TEMPLATE_INSNS, "Runaway generation of template, max instructions exceeded.");
             code[position++] = insn;
             if(insn == ABSTRACT_INSN) {
                 waiting_for_abstract_handler = true;
+                last_abstract_pos = position-1;
+            }
+            return *this;
+        }
+
+        constexpr InsnStream& operator << (AbstractResolver_t resolver) {
+            /*
+                handler is being provided for previous instruction
+                that may have generated an abstract instruction
+                add the marker if it was abstract, else do nothing
+            */
+            if(waiting_for_abstract_handler) {
+                CE_ASSERT(currentAbstractCount < MAX_ABSTRACT_MARKERS, "Too many abstract instruction were generated!");
+                waiting_for_abstract_handler = false;
+                markers[currentAbstractCount++] = {last_abstract_pos, resolver};
+            }
+            return *this;
+        }
+
+        constexpr uword_t size() const {
+            return position;
+        }
+        constexpr uword_t abstractCount() const {
+            return currentAbstractCount;
+        }
+
+        constexpr insn_t insnAt(uword_t index) const {
+            return code[index];
+        }
+        constexpr AbstractMarker markerAt(uword_t index) {
+            return markers[index];
+        }
+
+    };
+
+    template<auto& finalStream>
+    struct RTTemplate {
+        static constexpr uword_t template_code_size = finalStream.size();
+        static constexpr uword_t template_abstract_count = finalStream.abstractCount();
+        insn_t code[template_code_size];
+        AbstractMarker markers[template_abstract_count];
+
+        constexpr RTTemplate() : code(), markers() {
+            for(uword_t i = 0; i < template_code_size; ++i) {
+                code[i] = finalStream.insnAt(i);
+            }
+            for(uword_t i = 0; i < template_abstract_count; ++i) {
+                markers[i] = finalStream.markerAt(i);
+            }
+
+        }
+
+        template<uword_t current = 0>
+        inline insn_t dorepl(KnowableState* state, insn_t in_insn, uword_t idx) {
+            if(idx == markers[current]) {
+                return markers[current](state);
+            }
+            else {
+                if constexpr(current+1 < template_abstract_count) {
+                    return dorepl<current+1>(state, in_insn, idx);
+                }
+                else {
+                    return in_insn;
+                }
             }
         }
 
+        void emit(void* dataDestination) {
+            uword_t pc = reinterpret_cast<uword_t>(dataDestination);
+            KnowableState state{0};
 
+            for(uword_t i = 0; i < template_code_size; ++i, pc += 2) {
+                insn_t currentInsn = code[i];
+                if constexpr(template_abstract_count != 0) {
+                    state.pc = pc + 4;
+                    currentInsn = dorepl(state, currentInsn, i);
+                }
+                reinterpret_cast<insn_t*>(dataDestination)[i] = currentInsn;
+            }
+
+        }
     };
+
+    constexpr InsnStream hmmm() {
+        InsnStream result;
+        result << add(Register_e::r0, Register_e::r1);
+        result << bx(Register_e::lr);
+        return result;
+    }
+
+    template<InsnStream (*func)()>
+    constexpr auto produceTemplate() {
+        struct ResultHelper {
+            static constexpr InsnStream funcResult = func();
+            static constexpr RTTemplate<funcResult> result {};
+        };
+        return ResultHelper::result;
+    }
+
+    constexpr auto emitter = produceTemplate<hmmm>();
+
+
+    constexpr insn_t TEST = add(Register_e::r5, Register_e::pc, 48);
 
 }
